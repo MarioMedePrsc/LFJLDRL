a5746cbb8978d2e6c821291f3d129ed3b35df4f8
JesusGaMo <118152798+JesusGaMo@users.noreply.github.com>
2023-05-21 13:40:17 -0600

    Update inicio.php

--- Views/trabajadores/inicio.php
+++ Views/trabajadores/inicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=League Spartan' rel='stylesheet'>
-    <link rel="stylesheet" href="css/style02.css">
+    <link rel="stylesheet" href="../css/style02.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>LFJLDRL Interfaz 5</title>
 </head>
